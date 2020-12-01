ef47d214abf982d387e0bfbbbb7697e4f32eed4d
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2020-12-01 17:52:53 +0100

    :bug: Just after launching the app, fix when the store is not fullfilled yet.

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flip_card/store/cards/card_list_store.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:mobx/mobx.dart';
 import 'package:provider/provider.dart';
 
@@ -11,7 +12,7 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+class _HomePageState extends State<HomePage> {
   bool _found = false;
   CardListStore _cardList;
 
@@ -20,12 +21,17 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   void initState() {
     print("LOG: [_HomePageState] inistate : start");
     _cardList = Provider.of<CardListStore>(context, listen: false);
-    if (_cardList.list.value.isEmpty) {
-      print("LOG: [_HomePageState] inistate : la liste est vide, je vais 'fetch'");
-      _cardList.fetchCard();
-      print("LOG: [_HomePageState] inistate : " + _cardList.length.toString());
+    _cardList.list.status;
+    if (_cardList.list.status == FutureStatus.fulfilled) {
+      if (_cardList.list.value.isEmpty) {
+        print(
+            "LOG: [_HomePageState] inistate : la liste est vide, je vais 'fetch'");
+        _cardList.fetchCard();
+        print(
+            "LOG: [_HomePageState] inistate : " + _cardList.length.toString());
+      }
+      print("LOG: [_HomePageState] inistate : fin");
     }
-    print("LOG: [_HomePageState] inistate : fin");
     super.initState();
   }
 
@@ -59,39 +65,61 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-            children: [
-              Text(_cardList.list.result[_cardList.curentIndex.value].foreignWord.word),
-              if (!_found)
-                IconButton(
-                    icon: Icon(Icons.question_answer_outlined),
-                    onPressed: () {
-                      discoverWord();
-                    }),
-              Text('$_cardList.curentIndex.value'),
-              Text((() {
-                if (_found && !_cardList.isFinished) {
-                  return _cardList.list.result[_cardList.curentIndex.value].nativeWord.word;
-                } else {
-                  return "???";
-                }
-              })()),
-              if (_found && !_cardList.isFinished)
-                IconButton(
-                    icon: Icon(Icons.arrow_forward_rounded),
-                    onPressed: () {
-                      increaseCounter();
-                    }),
-              if (_cardList.isFinished)
-                IconButton(
-                    icon: Icon(Icons.restore),
-                    onPressed: () {
-                      resetIndex();
-                    }
-                    )
-            ]
-        )
+    final future = _cardList.list;
+    Widget _widgetDisplayed;
+    return Observer(
+      builder: (_) {
+        switch (future.status) {
+          case FutureStatus.pending:
+            _widgetDisplayed = const Center(
+              child: CircularProgressIndicator(),
+            );
+            break;
+          case FutureStatus.rejected:
+            // TODO: Handle this case.
+            break;
+          case FutureStatus.fulfilled:
+            _widgetDisplayed =
+                Scaffold(
+                body: Column(
+                    children: [
+                      Text(_cardList.list.result[_cardList.curentIndex.value].foreignWord.word),
+                      if (!_found)
+                        IconButton(
+                            icon: Icon(Icons.question_answer_outlined),
+                            onPressed: () {
+                              discoverWord();
+                            }),
+                      Text(_cardList.curentIndex.value.toString()),
+                      Text((() {
+                        if (_found && !_cardList.isFinished) {
+                          return _cardList.list.result[_cardList.curentIndex.value].nativeWord.word;
+                        } else {
+                          return "???";
+                        }
+                      })()),
+                      if (_found && !_cardList.isFinished)
+                        IconButton(
+                            icon: Icon(Icons.arrow_forward_rounded),
+                            onPressed: () {
+                              increaseCounter();
+                            }),
+                      if (_cardList.isFinished)
+                        IconButton(
+                            icon: Icon(Icons.restore),
+                            onPressed: () {
+                              resetIndex();
+                            }
+                        )
+                    ]
+                )
+            );
+            break;
+        }
+        return _widgetDisplayed;
+      }
     );
+
+    /*return ;*/
   }
 }
