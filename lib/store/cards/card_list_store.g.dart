6652f9044a33e1caf3f840bd196bf85116a77e6e
Croquelois <croquelois.adrien@gmail.com>
2021-01-02 18:55:52 +0100

    :sparkles: add learning screen

---
+++ lib/store/cards/card_list_store.g.dart
@@ -0,0 +1,112 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'card_list_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$CardListStore on _CardListStore, Store {
+  Computed<int> _$lengthComputed;
+
+  @override
+  int get length => (_$lengthComputed ??=
+          Computed<int>(() => super.length, name: '_CardListStore.length'))
+      .value;
+  Computed<bool> _$isFinishedComputed;
+
+  @override
+  bool get isFinished =>
+      (_$isFinishedComputed ??= Computed<bool>(() => super.isFinished,
+              name: '_CardListStore.isFinished'))
+          .value;
+
+  final _$curentIndexAtom = Atom(name: '_CardListStore.curentIndex');
+
+  @override
+  ObservableFuture<int> get curentIndex {
+    _$curentIndexAtom.reportRead();
+    return super.curentIndex;
+  }
+
+  @override
+  set curentIndex(ObservableFuture<int> value) {
+    _$curentIndexAtom.reportWrite(value, super.curentIndex, () {
+      super.curentIndex = value;
+    });
+  }
+
+  final _$listAtom = Atom(name: '_CardListStore.list');
+
+  @override
+  ObservableFuture<List<entity.CardEntity>> get list {
+    _$listAtom.reportRead();
+    return super.list;
+  }
+
+  @override
+  set list(ObservableFuture<List<entity.CardEntity>> value) {
+    _$listAtom.reportWrite(value, super.list, () {
+      super.list = value;
+    });
+  }
+
+  final _$_CardListStoreActionController =
+      ActionController(name: '_CardListStore');
+
+  @override
+  void resetIndex() {
+    final _$actionInfo = _$_CardListStoreActionController.startAction(
+        name: '_CardListStore.resetIndex');
+    try {
+      return super.resetIndex();
+    } finally {
+      _$_CardListStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  Future<void> fetchCard() {
+    final _$actionInfo = _$_CardListStoreActionController.startAction(
+        name: '_CardListStore.fetchCard');
+    try {
+      return super.fetchCard();
+    } finally {
+      _$_CardListStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void wordFinded() {
+    final _$actionInfo = _$_CardListStoreActionController.startAction(
+        name: '_CardListStore.wordFinded');
+    try {
+      return super.wordFinded();
+    } finally {
+      _$_CardListStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void wordMissed() {
+    final _$actionInfo = _$_CardListStoreActionController.startAction(
+        name: '_CardListStore.wordMissed');
+    try {
+      return super.wordMissed();
+    } finally {
+      _$_CardListStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+curentIndex: ${curentIndex},
+list: ${list},
+length: ${length},
+isFinished: ${isFinished}
+    ''';
+  }
+}
--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,7 +1,7 @@
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_flip_card/store/cards/card_list_store.dart';
 import 'package:flutter_flip_card/ui/widgets/utils/button/square_button.dart';
-import 'package:flutter_flip_card/ui/widgets/words/card_guess_widget.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:mobx/mobx.dart';
 import 'package:provider/provider.dart';
@@ -15,52 +15,196 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
   CardListStore _cardListStore;
+  bool _revealState;
+  bool _endOfSerie;
 
   @override
   void initState() {
     _cardListStore = Provider.of<CardListStore>(context, listen: false);
+    _revealState = false;
+    _endOfSerie = false;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     Widget _widgetDisplayed;
-    return Observer(
-      builder: (_) {
-        switch (_cardListStore.list.status) {
-          case FutureStatus.pending:
-            _widgetDisplayed = const Center(
-              child: CircularProgressIndicator(),
-            );
-            break;
-          case FutureStatus.rejected:
-            // TODO: Handle this case.
-            _widgetDisplayed =
-                Scaffold(
-                  body: Column(
+    return Observer(builder: (_) {
+      switch (_cardListStore.list.status) {
+        case FutureStatus.pending:
+          _widgetDisplayed = const Center(
+            child: CircularProgressIndicator(),
+          );
+          break;
+        case FutureStatus.rejected:
+          // TODO: Handle this case.
+          _widgetDisplayed = Scaffold(
+              body: Column(children: [
+            const Icon(Icons.warning_amber_outlined),
+            const Text('An error occured : the connection was rejected')
+          ]));
+          break;
+        case FutureStatus.fulfilled:
+          _widgetDisplayed = Scaffold(
+              resizeToAvoidBottomPadding: false,
+              body: Center(
+                  child: Column(children: [
+                Center(
+                    child: Text(
+                  // ignore: unnecessary_string_interpolations
+                  '${_endOfSerie ? 'Click to restart' : _revealState ? "Is it ok ?" : "Try to guess :"}',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w400, fontSize: 20),
+                )),
+                SizedBox(
+                    height: MediaQuery.of(context).size.height < 600 ? 5 : 20),
+                FlipCard(
+                  key: cardKey,
+                  flipOnTouch: false,
+                  front: Container(
+                      height: MediaQuery.of(context).size.height / 2,
+                      width: (MediaQuery.of(context).size.width / 100) * 70,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(15)),
+                        boxShadow: [
+                          const BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(2, 3), //(x,y)
+                            blurRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Text(
+                          _cardListStore
+                              .list
+                              .result[_cardListStore.curentIndex.value]
+                              .foreignWord
+                              .word,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 25),
+                        ),
+                      )),
+                  back: Container(
+                      height: MediaQuery.of(context).size.height / 2,
+                      width: (MediaQuery.of(context).size.width / 100) * 70,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(15)),
+                        boxShadow: [
+                          const BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(2, 3), //(x,y)
+                            blurRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                          child: Text(
+                        _cardListStore
+                            .list
+                            .result[_cardListStore.curentIndex.value]
+                            .nativeWord
+                            .word,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 25),
+                      ))),
+                ),
+                SizedBox(
+                    height: MediaQuery.of(context).size.height < 600 ? 20 : 45),
+                if (_endOfSerie)
+                  SquareButton(
+                    onPressed: _restartSerie,
+                    icon: const Icon(Icons.restore, size: 30),
+                    backgroundColor: Theme.of(context).cardColor,
+                    borderColor: Theme.of(context).primaryColor,
+                    width: _getButtonSize(),
+                    height: _getButtonSize(),
+                  )
+                else if (_revealState)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      const Icon(Icons.warning_amber_outlined),
-                      const Text('An error occured : the connection was rejected')
-                ]));
-            break;
-          case FutureStatus.fulfilled:
-            _widgetDisplayed =
-                Scaffold(
-                  resizeToAvoidBottomPadding: false,
-                body: Center(
-                  child: Column(
-                    children: [
-                        CardGuessWidget()
-
-                    ]
-                ))
-            );
-            break;
-        }
-        return _widgetDisplayed;
+                      SquareButton(
+                        onPressed: _successFind,
+                        icon: const Icon(Icons.clear, size: 30),
+                        backgroundColor: Theme.of(context).cardColor,
+                        borderColor: Theme.of(context).errorColor,
+                        width: _getButtonSize(),
+                        height: _getButtonSize(),
+                      ),
+                      SquareButton(
+                        onPressed: _errorFind,
+                        icon: const Icon(Icons.check, size: 30),
+                        backgroundColor: Theme.of(context).cardColor,
+                        borderColor: Theme.of(context).indicatorColor,
+                        width: _getButtonSize(),
+                        height: _getButtonSize(),
+                      )
+                    ],
+                  )
+                else
+                  SquareButton(
+                    onPressed: _revealWord,
+                    icon: const Icon(Icons.remove_red_eye, size: 30),
+                    backgroundColor: Theme.of(context).cardColor,
+                    borderColor: Theme.of(context).primaryColor,
+                    width: _getButtonSize(),
+                    height: _getButtonSize(),
+                  )
+              ])));
       }
+      return _widgetDisplayed;
+    });
+  }
 
-    );
+  void _revealWord() {
+    setState(() {
+      _revealState = true;
+    });
+    cardKey.currentState.toggleCard();
+  }
+
+  void _successFind() {
+    if (!_cardListStore.isFinished) {
+      _cardListStore.wordFinded();
+      setState(() {
+        _revealState = false;
+      });
+    } else {
+      setState(() {
+        _endOfSerie = true;
+      });
+    }
+  }
+
+  void _errorFind() {
+    if (!_cardListStore.isFinished) {
+      _cardListStore.wordMissed();
+      setState(() {
+        _revealState = false;
+      });
+    } else {
+      setState(() {
+        _endOfSerie = true;
+      });
+    }
+  }
+
+  void _restartSerie() {
+    _cardListStore.resetIndex();
+    setState(() {
+      _endOfSerie = false;
+      _revealState = false;
+    });
+  }
+
+  double _getButtonSize() {
+    return MediaQuery.of(context).size.height < 600 ? 45 : 55;
   }
 }
