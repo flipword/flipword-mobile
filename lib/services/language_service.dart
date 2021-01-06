d45f4363b033f1c8910f4cdc93da7e0462f55ef5
Antoine <aberthier15@gmail.com>
2021-01-06 16:26:29 +0100

    :bug: fix default foreignLanguage

--- lib/services/language_service.dart
+++ lib/services/language_service.dart
@@ -7,7 +7,7 @@ class LanguageService {
   LanguageService._privateConstructor();
 
   Language nativeLanguage = LanguagePickerUtils.getLanguageByIsoCode('fr');
-  Language foreignLanguage = LanguagePickerUtils.getLanguageByIsoCode('fr');
+  Language foreignLanguage = LanguagePickerUtils.getLanguageByIsoCode('en');
 
   static final LanguageService _instance = LanguageService._privateConstructor();
   static LanguageService get instance => _instance;
