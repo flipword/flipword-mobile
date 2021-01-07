42086b924d449361305ce7d242b333ccb3d62415
Croquelois <croquelois.adrien@gmail.com>
2021-01-07 21:02:09 +0100

    :art: remove space and reformat

--- lib/ui/widgets/utils/button/icon_text_button.dart
+++ lib/ui/widgets/utils/button/icon_text_button.dart
@@ -1,50 +1,41 @@
-
 import 'package:flutter/material.dart';
 
 class IconTextButton extends StatelessWidget {
-  const IconTextButton({Key key,
-    this.icon,
-    this.text,
-    this.backgroundColor,
-    this.width ,
-    @required this.onPressed} ): super(key: key);
+  const IconTextButton(
+      {Key key,
+      this.icon,
+      this.text,
+      this.backgroundColor,
+      this.width,
+      @required this.onPressed})
+      : super(key: key);
 
   final IconData icon;
   final String text;
   final Color backgroundColor;
   final VoidCallback onPressed;
-  final double width ;
+  final double width;
+
   @override
   Widget build(BuildContext context) {
     return Container(
         width: width,
         child: MaterialButton(
-
-          splashColor: Theme.of(context).accentColor,
-          elevation: 10,
-
-          highlightElevation: 0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(5),
-          ),
-          onPressed: () => {onPressed()},
-          color: Theme.of(context).primaryColor,
-          padding: const EdgeInsets.only(
-              top:5,
-              right: 10,
-              bottom: 5,
-              left: 10),
-          child:
-            Opacity(
+            splashColor: Theme.of(context).accentColor,
+            elevation: 10,
+            highlightElevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            onPressed: () => {onPressed()},
+            color: Theme.of(context).primaryColor,
+            padding:
+                const EdgeInsets.only(top: 5, right: 10, bottom: 5, left: 10),
+            child: Opacity(
               opacity: 0.7,
               child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(text),
-                    Icon(icon, size: 30)
-                  ]),
-            )
-        )
-    );
+                  children: [Text(text), Icon(icon, size: 30)]),
+            )));
   }
 }
