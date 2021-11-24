a2e72b1a27333e72c17df2daee5224e4f6befd2c
croquelois <croquelois.adrien@gmail.com>
2021-11-24 23:06:29 +0100

    :bug: upgrade mobx to fix null safety

--- lib/store/setting/setting_store.g.dart
+++ lib/store/setting/setting_store.g.dart
@@ -9,7 +9,7 @@ part of 'setting_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$SettingStore on _SettingStore, Store {
-  Computed<bool> _$isReverseLanguageComputed;
+  Computed<bool>? _$isReverseLanguageComputed;
 
   @override
   bool get isReverseLanguage => (_$isReverseLanguageComputed ??= Computed<bool>(
